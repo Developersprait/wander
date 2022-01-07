24e295f9b8e0b93d00f24597c507c165e9d3b61f
Leon Antoshyn <Doctorsprait@gmail.com>
2022-01-07 10:49:10 +0300

    minor slider added

---
+++ js/slider2.js
@@ -0,0 +1,18 @@
+$('#slider').on('init reInit',function(event,slick){
+    var amount = slick.slideCount;
+    $('#range').attr('max',amount);
+  });
+  
+  $('#slider').on('afterChange',function(e,slick,currentSlide){
+    $('#range').val(currentSlide+1);
+  });
+  
+  $('#range').on('input change',function(){
+    $('#slider').slick('slickGoTo',this.value-1);
+  });
+  
+  $('#slider').slick({
+    slidesToShow:2,
+    arrows:false,
+    dots:false
+  });
