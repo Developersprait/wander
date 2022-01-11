ee94ddd94713b7aebf936fc67f2ab4116abc63b9
Leon Antoshyn <Doctorsprait@gmail.com>
2022-01-11 21:59:40 +0300

    2nd page started

---
+++ js/slider1.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    //slider
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        const slides = document.getElementsByClassName("mySlides");
+
+        if (!slides.length) {
+            return;
+        }
+
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+
+        slideIndex += 1;
+
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+        slides[slideIndex - 1].style.display = "block";
+        setTimeout(showSlides, 4000);
+    }
+});
