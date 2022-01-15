a398f7b74d87680b81fc3ea18dc76a962c064f68
Leon Antoshyn <Doctorsprait@gmail.com>
2022-01-15 11:04:26 +0300

    tour section finished

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function fix() {
         document.addEventListener('scroll', () => {
-            if (window.scrollY > 800) {
+            if (window.scrollY > 100) {
                 document.querySelector('.shedule__btn').style.cssText = 'position: fixed; right: 0; top:84%; box-shadow: -14px -18px 19px 7px rgb(255 255 255 / 43%):';
                 console.log('done');
             }
