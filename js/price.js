5e154fabc447a054696203858cc4d27bad03c10b
Leon Antoshyn <Doctorsprait@gmail.com>
2022-01-22 18:46:21 +0300

    price section mobile version edited

---
+++ js/price.js
@@ -0,0 +1,24 @@
+function price() {
+    if (window.screen.width >= 992) {
+        document.querySelector('.price__inner').innerHTML = `
+        <div class="price__logo"></div>
+        <div class="price__descr">Organizator: <br>
+                        ( agencja podróży ekstremalnych "wędrowcy")
+        </div>
+        <div class="price__name">WANDERERS</div>
+        `;
+    }else if (window.screen.width < 992) {
+        document.querySelector('.price__inner').innerHTML = `
+        <div class="price__descr">Organizator: <br>
+                        ( agencja podróży ekstremalnych "wędrowcy")
+        </div>
+        <div class="price__box">
+            <div class="price__box-left">
+                <div class="price__logo"></div>
+                <div class="price__name">WANDERERS</div>
+            </div>
+            <div class="price__box-right"></div>
+        </div>    
+        `;}
+}
+price();
